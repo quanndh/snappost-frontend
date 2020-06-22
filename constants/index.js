80dad2fe2caada891e9f32ef75b908202f7c2831
Quân Nguyễn <wantmorexx9x@gmail.com>
2020-06-22 17:05:36 +0700

    .

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,5 @@
 export const FacebookAppId = '249068886329486';
-export const GoogleClientId = "624774030832-hnl25u4l503s9s6h0kb52ht3bp0qubbg.apps.googleusercontent.com"
+export const GoogleClientId = "750065301705-mk6qe7fot41p2bo1s8mnqbl2otaquglm.apps.googleusercontent.com"
 export default {
     apiHost: "http://35.232.6.168:1337/api"
 }
